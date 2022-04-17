64fe9221d87cc5c0ce032765e6b99067fd81bd82
LLeon360 <103869590+LLeon360@users.noreply.github.com>
2022-04-16 20:45:59 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools import setup
+
+setup(
+  name='LeonPackage',
+  author='Leon Liu',
+  author_email="liuleon360@gmail.com",
+  version='1.0.0',
+  license="MIT license",
+  description="Sample package",
+  packages=["Model"],
+)
