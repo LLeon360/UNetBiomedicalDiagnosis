6f1b299754d9e37c2bbd4c389de941002271692f
LLeon360 <103869590+LLeon360@users.noreply.github.com>
2022-04-20 00:12:38 -0700

    Update modules.py

--- Model/modules.py
+++ Model/modules.py
@@ -47,63 +47,63 @@ class Unet:
 
     return next_layer, skip_connection
 
-def DecoderMiniBlock(prev_layer_input, skip_layer_input, n_filters=32):
-  up = Conv2DTranspose(
-                n_filters,
-                (3,3),   
-                strides=(2,2),
-                padding='same')(prev_layer_input)
+  def DecoderMiniBlock(prev_layer_input, skip_layer_input, n_filters=32):
+    up = Conv2DTranspose(
+                  n_filters,
+                  (3,3),   
+                  strides=(2,2),
+                  padding='same')(prev_layer_input)
 
-  merge = concatenate([up, skip_layer_input], axis=3)
-  
-  conv = Conv2D(n_filters, 
-                3,     
-                activation='relu',
-                padding='same')(merge)
-  conv = Conv2D(n_filters,
-                3,  
-                activation='relu', 
-                padding='same')(conv)
-  return conv
+    merge = concatenate([up, skip_layer_input], axis=3)
 
-def BuildModel(input_size=(128, 128, 1), n_filters=32, n_classes=2, n_layers = 4, dropout=0.3):
+    conv = Conv2D(n_filters, 
+                  3,     
+                  activation='relu',
+                  padding='same')(merge)
+    conv = Conv2D(n_filters,
+                  3,  
+                  activation='relu', 
+                  padding='same')(conv)
+    return conv
 
-  inputs = Input(input_size)
-  
-  eblocks = []
-  #n_layers is encoder layers
-  # Encoder includes multiple convolutional mini blocks with different maxpooling, dropout and filter parameters
-  # Observe that the filters are increasing as we go deeper into the network which will increasse the # channels of the image 
-  eblocks.append(EncoderMiniBlock(inputs, n_filters,dropout_prob=0, max_pooling=True))
-  for i in range(n_layers-1):
-    dropout = 0
-    if(i > 1):
-      dropout = 0.3
-    eblocks.append(EncoderMiniBlock(eblocks[-1][0], n_filters*(2**(i+1)), dropout_prob=dropout, max_pooling=True))
+  def BuildModel(input_size=(128, 128, 1), n_filters=32, n_classes=2, n_layers = 4, dropout=0.3):
 
-  #intermediate latent also put in encoder
-  eblocks.append(EncoderMiniBlock(eblocks[-1][0], n_filters*(2**(n_layers)), dropout_prob=0, max_pooling=False)) 
-  
-  dblocks = []
-  dblocks.append(DecoderMiniBlock(eblocks[-1][0], eblocks[-2][1],  n_filters*(2**(n_layers-1))))
-  for i in range(n_layers-1): 
-    dblocks.append(DecoderMiniBlock(dblocks[-1], eblocks[-i-3][1],  n_filters*(2**(n_layers-2-i))))
+    inputs = Input(input_size)
 
-  # Complete the model with 1 3x3 convolution layer (Same as the prev Conv Layers)
-  # Followed by a 1x1 Conv layer to get the image to the desired size. 
-  # Observe the number of channels will be equal to number of output classes
-  conv_last = Conv2D(n_filters,
-                3,
-                activation='relu',
-                padding='same')(dblocks[-1])
+    eblocks = []
+    #n_layers is encoder layers
+    # Encoder includes multiple convolutional mini blocks with different maxpooling, dropout and filter parameters
+    # Observe that the filters are increasing as we go deeper into the network which will increasse the # channels of the image 
+    eblocks.append(EncoderMiniBlock(inputs, n_filters,dropout_prob=0, max_pooling=True))
+    for i in range(n_layers-1):
+      dropout = 0
+      if(i > 1):
+        dropout = 0.3
+      eblocks.append(EncoderMiniBlock(eblocks[-1][0], n_filters*(2**(i+1)), dropout_prob=dropout, max_pooling=True))
 
-  out = Conv2D(n_classes, 1, 
-                activation='softmax', padding='same')(conv_last)
-  
-  # Define the model
-  model = tf.keras.Model(inputs=inputs, outputs=out)
+    #intermediate latent also put in encoder
+    eblocks.append(EncoderMiniBlock(eblocks[-1][0], n_filters*(2**(n_layers)), dropout_prob=0, max_pooling=False)) 
 
-  return model
+    dblocks = []
+    dblocks.append(DecoderMiniBlock(eblocks[-1][0], eblocks[-2][1],  n_filters*(2**(n_layers-1))))
+    for i in range(n_layers-1): 
+      dblocks.append(DecoderMiniBlock(dblocks[-1], eblocks[-i-3][1],  n_filters*(2**(n_layers-2-i))))
+
+    # Complete the model with 1 3x3 convolution layer (Same as the prev Conv Layers)
+    # Followed by a 1x1 Conv layer to get the image to the desired size. 
+    # Observe the number of channels will be equal to number of output classes
+    conv_last = Conv2D(n_filters,
+                  3,
+                  activation='relu',
+                  padding='same')(dblocks[-1])
+
+    out = Conv2D(n_classes, 1, 
+                  activation='softmax', padding='same')(conv_last)
+
+    # Define the model
+    model = tf.keras.Model(inputs=inputs, outputs=out)
+
+    return model
 
 #   def UNet(input_size=(128, 128, 1), 
 #            n_filters=32, 
