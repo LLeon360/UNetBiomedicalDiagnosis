4ffbd9b253442b2bade70bcecde96ee77678ebbb
Leon Liu <103869590+LLeon360@users.noreply.github.com>
2024-06-06 18:11:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-  name='LeonPackage',
+  name='UNetBiomedicalDiagnosis',
   author='Leon Liu',
   author_email="liuleon360@gmail.com",
   version='1.0.0',
   license="MIT license",
-  description="Sample package",
+  description="Package for UNet image segmentation",
   packages=["Model"],
 )
